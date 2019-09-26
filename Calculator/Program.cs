02913dce472126e71e5c1271f9f12820c5b7aea3
MichalG-w <michalgrzegorzgajowy@gmail.com>
2019-09-26 13:29:50 +0200

    what new

---
+++ Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("1. Dodawanie");
+            Console.WriteLine("2. Odejmowanie");
+            Console.WriteLine("3. Wyjscie");
+            int a = int.Parse(Console.ReadLine());
+            if(a == 3)
+            {
+                Console.WriteLine("wyjscie");
+            }
+                
+            
+              
+            
+               
+            
+
+        }
+    }
+}
