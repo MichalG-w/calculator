020fe8c1d0a43e794f0e114fad0bf2913a170ecc
ullanor <pawel.nowacki95@gmail.com>
2019-09-27 18:29:57 +0200

    blabla

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -24,6 +24,7 @@ namespace Calculator
             if(a == 3)
             {
                 Console.WriteLine("wyjscie");
+                Console.ReadKey();
             }
                 
             
