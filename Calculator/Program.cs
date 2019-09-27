a998b3ae6182d412c34816af7d07ab4627268123
ullanor <pawel.nowacki95@gmail.com>
2019-09-27 18:31:58 +0200

    new changes by Ullanor

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,23 +11,25 @@ namespace Calculator
         static void Main(string[] args)
         {
 
-
-            Console.WriteLine("0. Nie dziel prze zero");
-            Console.WriteLine("1. Dodawanie");
-            Console.WriteLine("2. Odejmowanie");
-            Console.WriteLine("3. Wyjscie");
-            Console.WriteLine("4. Mnożenie");
-            
-            Console.WriteLine("5. Dzielenie");
-            Console.WriteLine("6. Potęgowanie");
-            int a = int.Parse(Console.ReadLine());
-            if(a == 3)
+            while (true)
             {
-                Console.WriteLine("wyjscie");
-                Console.ReadKey();
+                Console.WriteLine("999. Wyjscie");
+                Console.WriteLine("0. Nie dziel prze zero");
+                Console.WriteLine("1. Dodawanie");
+                Console.WriteLine("2. Odejmowanie");
+                Console.WriteLine("4. Mnożenie");
+
+                Console.WriteLine("5. Dzielenie");
+                Console.WriteLine("6. Potęgowanie");
+                int a = int.Parse(Console.ReadLine());
+                if (a == 999)
+                {
+                    Console.WriteLine("wyjscie");
+                    Console.ReadKey();
+                    break;
+                }
+
             }
-                
-            
               
             
                
